030568869893f82d49da1833167a2ea917d2444c
cjpick <cpick@cosmocaters.com>
2018-03-07 16:15:01 -0800

    Adding CommandLine shell command runner

--- Sources/CommandLine+Extras.swift
+++ Sources/CommandLine+Extras.swift
@@ -20,4 +20,28 @@ extension CommandLine {
         }
         return arguments
     }
+    
+    public static func printShell(_ launchPath: String, arguments: [String] = []) {
+        let output = shell(launchPath, arguments:arguments)
+        
+        if (output != nil) {
+            print(output!)
+        }
+    }
+    
+    public static func shell(_ launchPath: String, arguments: [String] = []) -> String? {
+        
+        let task = Process()
+        task.launchPath = launchPath
+        task.arguments = arguments
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String? = String(data: data, encoding: String.Encoding.utf8)
+        
+        return output
+    }
 }
